280e7223d1c465d234ed495ed0886da60715194d
GuChen <1023797278@qq.com>
2023-03-05 17:08:50 +0800

    mem-cache: for test
    Change-Id: I47a99ae72d0f2bb95d734500a796e792beb5e5eb

--- parallel/dense.cc
+++ parallel/dense.cc
@@ -1,64 +1,68 @@
-#include <iostream>
-#include <assert.h>
-#include <ctime>
-#include <cmath>
 #include <pthread.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
 #include <cstring>
+#include <ctime>
+#include <iostream>
+#include <sstream>
 
 using namespace std;
 
 pthread_mutex_t mutex;
 
-typedef struct _thread_arg {
+typedef struct _thread_arg
+{
     int matrix_size;
     int blocking_factor;
-    int ** x_matrix;
-    int ** y_matrix;
-    int ** z_matrix;  // matrix multiplication
+    int **x_matrix;
+    int **y_matrix;
+    int **z_matrix; // matrix multiplication
 
     int blocks_num;
-    int * block_id;  // workload: which blocks the thread should handle
+    int *block_id; // workload: which blocks the thread should handle
 
     // for core binding:
-    char tag[10];  // thread name(tag)
-    pthread_t pid;  // thread id
-    int core_id;  // bind thread to core id
-    void * (* run)(void * args);  // thread routine(non-binding part)
+    char tag[10];             // thread name(tag)
+    pthread_t pid;            // thread id
+    int core_id;              // bind thread to core id
+    void *(*run)(void *args); // thread routine(non-binding part)
 
-    _thread_arg() {
+    _thread_arg()
+    {
         matrix_size = blocking_factor = blocks_num = 0;
-        x_matrix = y_matrix = z_matrix = nullptr;
-        block_id = nullptr;
+        x_matrix = y_matrix = z_matrix = NULL;
+        block_id = NULL;
 
         // for core binding:
-        run = nullptr;
+        run = NULL;
         pid = core_id = 0;
     };
 } thread_arg;
 
-int ** gen_matrix(int matrix_size)
+int **gen_matrix(int matrix_size)
 {
-    int ** matrix = new int * [matrix_size];
+    int **matrix = new int *[matrix_size];
     for (int i = 0; i < matrix_size; ++i)
-        matrix[i] = new int [matrix_size];
+        matrix[i] = new int[matrix_size];
     for (int i = 0; i < matrix_size; ++i)
         for (int j = 0; j < matrix_size; ++j)
             matrix[i][j] = rand() % 2;
     return matrix;
 }
 
-void destructor(int ** matrix, int matrix_size)
+void destructor(int **matrix, int matrix_size)
 {
     for (int i = 0; i < matrix_size; ++i)
-        delete [] matrix[i];
-    delete [] matrix;
+        delete[] matrix[i];
+    delete[] matrix;
 }
 
-void print_matrix(int ** matrix, int matrix_size)
+void print_matrix(int **matrix, int matrix_size)
 {
-    for (int i = 0; i < matrix_size; ++i) {
+    for (int i = 0; i < matrix_size; ++i)
+    {
         for (int j = 0; j < matrix_size; ++j)
             cout << matrix[i][j] << " ";
         cout << endl;
@@ -66,11 +70,12 @@ void print_matrix(int ** matrix, int matrix_size)
 }
 
 // for correctness
-bool benchmark(int matrix_size, int ** x_matrix, int ** y_matrix, int ** z_matrix)
+bool benchmark(int matrix_size, int **x_matrix, int **y_matrix, int **z_matrix)
 {
-    int ** benchmark_matrix = gen_matrix(matrix_size);
+    int **benchmark_matrix = gen_matrix(matrix_size);
     for (int i = 0; i < matrix_size; ++i)
-        for (int j = 0; j < matrix_size; ++j) {
+        for (int j = 0; j < matrix_size; ++j)
+        {
             int r = 0;
             for (int k = 0; k < matrix_size; ++k)
                 r += y_matrix[i][k] * z_matrix[k][j];
@@ -83,7 +88,8 @@ bool benchmark(int matrix_size, int ** x_matrix, int ** y_matrix, int ** z_matri
 
     for (int i = 0; i < matrix_size; ++i)
         for (int j = 0; j < matrix_size; ++j)
-            if (benchmark_matrix[i][j] != x_matrix[i][j]) {
+            if (benchmark_matrix[i][j] != x_matrix[i][j])
+            {
                 destructor(benchmark_matrix, matrix_size);
                 return false;
             }
@@ -94,8 +100,9 @@ bool benchmark(int matrix_size, int ** x_matrix, int ** y_matrix, int ** z_matri
 }
 
 // wrapper in each thread for core binding
-static void * wrapper(void * args) {
-    thread_arg * obj = (thread_arg *) args;
+static void *wrapper(void *args)
+{
+    thread_arg *obj = (thread_arg *)args;
     pthread_setname_np(obj->pid, obj->tag);
     cpu_set_t cpuset;
     CPU_ZERO(&cpuset);
@@ -103,34 +110,40 @@ static void * wrapper(void * args) {
     pthread_setaffinity_np(obj->pid, sizeof(cpu_set_t), &cpuset);
     cout << obj->tag << " starts running..." << endl;
     obj->run(args);
-    return nullptr;
+    return NULL;
 }
 
-static void * thread_routine(void * arg) {
-    thread_arg * argu = (thread_arg *) arg;  // parse the arguments
+static void *thread_routine(void *arg)
+{
+    thread_arg *argu = (thread_arg *)arg; // parse the arguments
     int matrix_size = argu->matrix_size;
     int blocking_factor = argu->blocking_factor;
-    int ** x_matrix = argu->x_matrix;
-    int ** y_matrix = argu->y_matrix;
-    int ** z_matrix = argu->z_matrix;
+    int **x_matrix = argu->x_matrix;
+    int **y_matrix = argu->y_matrix;
+    int **z_matrix = argu->z_matrix;
     int blocks_num = argu->blocks_num;
-    int * block_id = argu->block_id;
+    int *block_id = argu->block_id;
 
-    for (int i = 0; i < blocks_num; ++i) {  // each iteration handles one block
+    for (int i = 0; i < blocks_num; ++i)
+    { // each iteration handles one block
         // calculate the starting row and column: jj, kk
         int id = block_id[i];
         int blocks_per_row = matrix_size / blocking_factor;
         int jj = floor(id / blocks_per_row) * blocking_factor;
         int kk = (id % blocks_per_row) * blocking_factor;
 
-        for (int i = 0; i < matrix_size; ++i)  // all rows in x_matrix are affected
-            for (int k = kk; k < kk + blocking_factor; k++) {  // update elements in certain columns
+        for (int i = 0; i < matrix_size; ++i)
+            // all rows in x_matrix are affected
+            for (int k = kk; k < kk + blocking_factor; k++)
+            { // update elements in certain columns
                 int r = 0;
-                for (int j = 0; j < blocking_factor; j++)  // number of elements to be added
+                for (int j = 0; j < blocking_factor; j++)
+                    // number of elements to be added
                     r += y_matrix[i][jj + j] * z_matrix[jj + j][k];
                 pthread_mutex_lock(&mutex);
                 x_matrix[i][k] += r;
-                pthread_mutex_unlock(&mutex);  // fine-grained locks for refinement
+                pthread_mutex_unlock(&mutex);
+                // fine-grained locks for refinement
             }
     }
 
@@ -138,13 +151,19 @@ static void * thread_routine(void * arg) {
     // for (;;)
     //     sleep(1);
 
-    return nullptr;
+    return NULL;
 }
-
-int main(int argc, char ** argv)
+int stoi(char *a)
+{
+    stringstream b(a);
+    int x;
+    b >> x;
+    return x;
+}
+int main(int argc, char **argv)
 {
     // usage: ./dense.out [matrix size] [blocking factor] [threads number] [binding]
-    assert(argc == 5);  // then, parse the parameters
+    assert(argc == 5); // then, parse the parameters
     int matrix_size = stoi(argv[1]);
     int blocking_factor = stoi(argv[2]);
     int threads_number = stoi(argv[3]);
@@ -152,26 +171,30 @@ int main(int argc, char ** argv)
     assert(matrix_size % blocking_factor == 0);
 
     cout << "=== INPUT ===" << endl;
-    cout << "matrix size: " << matrix_size << " blocking factor: " << blocking_factor << " threads number: " << threads_number << endl << endl;
+    cout << "matrix size: " << matrix_size << " blocking factor: "
+         << blocking_factor
+         << " threads number: " << threads_number << endl
+         << endl;
 
     cout << "=== CPU_NUM ===" << endl;
-    cout << "system cpu number: " << sysconf(_SC_NPROCESSORS_CONF) << endl << endl;
+    cout << "system cpu number: " << sysconf(_SC_NPROCESSORS_CONF) << endl
+         << endl;
 
-    srand(time(nullptr));  // matrix elements are random
+    srand(time(NULL)); // matrix elements are random
 
-    pthread_mutex_init(&mutex, nullptr);
+    pthread_mutex_init(&mutex, NULL);
 
-    int ** y_matrix = gen_matrix(matrix_size);
+    int **y_matrix = gen_matrix(matrix_size);
     cout << "=== Y_MATRIX ===" << endl;
     print_matrix(y_matrix, matrix_size);
     cout << endl;
 
-    int ** z_matrix = gen_matrix(matrix_size);
+    int **z_matrix = gen_matrix(matrix_size);
     cout << "=== Z_MATRIX ===" << endl;
     print_matrix(z_matrix, matrix_size);
     cout << endl;
 
-    int ** x_matrix = gen_matrix(matrix_size);  // multiplication: X = Y * Z
+    int **x_matrix = gen_matrix(matrix_size); // multiplication: X = Y * Z
 
     for (int i = 0; i < matrix_size; ++i)
         for (int j = 0; j < matrix_size; ++j)
@@ -193,10 +216,11 @@ int main(int argc, char ** argv)
 
     int num_of_blocks = (matrix_size / blocking_factor) * (matrix_size / blocking_factor);
     int blocks_per_thread = floor(num_of_blocks / threads_number);
-    thread_arg * args = new thread_arg [threads_number];  // thread argument
+    thread_arg *args = new thread_arg[threads_number]; // thread argument
 
     // prepare arguments
-    for (int i = 0; i < threads_number; ++i) {
+    for (int i = 0; i < threads_number; ++i)
+    {
         args[i].matrix_size = matrix_size;
         args[i].blocking_factor = blocking_factor;
         args[i].x_matrix = x_matrix;
@@ -207,31 +231,37 @@ int main(int argc, char ** argv)
             args[i].blocks_num = blocks_per_thread;
         else
             args[i].blocks_num = num_of_blocks - blocks_per_thread * (threads_number - 1);
-        
-        args[i].block_id = new int [args[i].blocks_num];  // then, assign block id to thread
-        for (int j = 0; j < args[i].blocks_num; ++j) {
+
+        args[i].block_id = new int[args[i].blocks_num];
+        // then, assign block id to thread
+        for (int j = 0; j < args[i].blocks_num; ++j)
+        {
             args[i].block_id[j] = i * blocks_per_thread + j;
         }
 
         // for core binding:
         args[i].run = thread_routine;
-        strcpy(args[i].tag, ("id-" + to_string(i)).c_str());
+        string x;
+        stringstream t(x);
+        t << i;
+        strcpy(args[i].tag, ("id-" + x).c_str());
         if (binding)
             args[i].core_id = i % sysconf(_SC_NPROCESSORS_CONF);
         else
-            args[i].core_id = sysconf(_SC_NPROCESSORS_CONF);  // policy to assign threads to different cores
+            args[i].core_id = sysconf(_SC_NPROCESSORS_CONF);
+        // policy to assign threads to different cores
     }
 
     for (int i = 0; i < threads_number; ++i)
-        pthread_create(&args[i].pid, nullptr, wrapper, (void *) & args[i]);
+        pthread_create(&args[i].pid, NULL, wrapper, (void *)&args[i]);
 
     for (int i = 0; i < threads_number; ++i)
-        pthread_join(args[i].pid, nullptr);
+        pthread_join(args[i].pid, NULL);
 
     for (int i = 0; i < threads_number; ++i)
-        delete [] args[i].block_id;
+        delete[] args[i].block_id;
 
-    delete [] args;
+    delete[] args;
 
     pthread_mutex_destroy(&mutex);
 
